1cac090e5419df50fd31c12038503e599a53ffe3
Habidhossen <habidhossen2@gmail.com>
2022-02-17 17:27:17 +0600

    finish

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
+// get input value function
+function getInput(inputId) {
+  const input = document.getElementById(inputId).value;
+  return input;
+}
+// alert message function
+function alertMessage(inputId) {
+  document.getElementById(inputId).style.display = "block";
+}
+
+// Calculate Button
 document.getElementById("calculate-btn").addEventListener("click", function () {
-  // select input value
-  const inputIncome = document.getElementById("inputIncome").value;
-  const inputFood = document.getElementById("inputFood").value;
-  const inputRent = document.getElementById("inputRent").value;
-  const inputClothes = document.getElementById("inputClothes").value;
-  // get input value & convert to a float value
+  // get value
+  const inputIncome = getInput("inputIncome");
+  const inputFood = getInput("inputFood");
+  const inputRent = getInput("inputRent");
+  const inputClothes = getInput("inputClothes");
+  // convert value
   const incomeValue = parseFloat(inputIncome);
   const foodvalue = parseFloat(inputFood);
   const rentValue = parseFloat(inputRent);
@@ -16,25 +27,24 @@ document.getElementById("calculate-btn").addEventListener("click", function () {
     inputRent == "" ||
     inputClothes == ""
   ) {
-    document.getElementById("alert-1").style.display = "block";
+    alertMessage("alert-1");
   } else if (
     incomeValue < 0 ||
     foodvalue < 0 ||
     rentValue < 0 ||
     clothesValue < 0
   ) {
-    document.getElementById("alert-2").style.display = "block";
+    alertMessage("alert-2");
   } else {
     const totalExpenses = foodvalue + rentValue + clothesValue;
     const balance = incomeValue - totalExpenses;
 
     if (incomeValue < totalExpenses) {
-      document.getElementById("alert-3").style.display = "block";
+      alertMessage("alert-3");
     } else {
       // get total expenses value & set value
       const totalExpensesValue = document.getElementById("total-expenses");
       totalExpensesValue.innerText = totalExpenses;
-
       // get balance value & set value
       const balanceValue = document.getElementById("balance");
       balanceValue.innerText = balance;
@@ -44,34 +54,33 @@ document.getElementById("calculate-btn").addEventListener("click", function () {
 
 // Save Button
 document.getElementById("save-btn").addEventListener("click", function () {
-  // select input value
-  const inputIncome = document.getElementById("inputIncome").value;
+  // get value
+  const inputIncome = getInput("inputIncome");
+  const inputPercentage = getInput("inputPercentage");
+  // convert value
   const incomeValue = parseFloat(inputIncome);
-  const inputPercentage = document.getElementById("inputPercentage").value;
   const percentageValue = parseFloat(inputPercentage);
-
   // check condition
   if (inputIncome == "" || inputPercentage == "") {
-    document.getElementById("alert-1").style.display = "block";
+    alertMessage("alert-1");
   } else if (incomeValue < 0 || inputPercentage < 0) {
-    document.getElementById("alert-2").style.display = "block";
+    alertMessage("alert-2");
   } else {
-    //   percentage calculation
+    // percentage calculation
     const savingAmount = (incomeValue * percentageValue) / 100;
     // get balance value & set value
     const balanceValue = document.getElementById("balance").innerText;
     const balance = parseFloat(balanceValue);
 
     if (balance < savingAmount) {
-      document.getElementById("alert-3").style.display = "block";
+      alertMessage("alert-3");
     } else {
-      //   set percentage value
+      // set percentage value
       const savingAmountValue = document.getElementById("saving-amount");
       savingAmountValue.innerText = savingAmount;
-
+      // calculation
       const remainingBalance = balance - savingAmount;
-
-      //   set remaining balance value
+      // set remaining balance value
       const remainingBalanceValue =
         document.getElementById("remaining-balance");
       remainingBalanceValue.innerText = remainingBalance;
